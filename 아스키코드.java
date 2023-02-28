e0b44da2667445d0c89804441172fb988be24f3b
Hyoung Jin <drj9802@gmail.com>
2023-03-01 01:41:42 +0900

    p11654 문자열 - 아스키 코드

--- 아스키코드.java
+++ 아스키코드.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class Main{
+public class 아스키코드{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int c = br.readLine().charAt(0);
