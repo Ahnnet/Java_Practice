a182cf7960c7223a4c3cbec4ebd13c1b799087ba
Hyoung Jin <drj9802@gmail.com>
2023-03-01 02:32:11 +0900

    p2743 문자열 - 단어 길이 재기

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int c = br.readLine().charAt(0);
-        System.out.println(c);
+        String str = br.readLine();
+        System.out.println(str.length());
     }
 }
