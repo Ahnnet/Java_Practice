90d3929f55f6bcc32c9697eeba88a9407b9c5ec4
Hyoung Jin <drj9802@gmail.com>
2023-03-02 23:38:12 +0900

    p11720 문자열 - 문자열의 합

--- 문자열의합.java
+++ 문자열의합.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
 
-public class Main{
+public class 문자열의합{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
