2722f9dfdb8d317a8cb41e179b4652763db76c35
Hyoung Jin <drj9802@gmail.com>
2023-03-05 23:05:20 +0900

    _

--- Main.java
+++ Main.java
@@ -10,33 +10,5 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-
-        for(int i=0;i<n;i++){
-
-
-            String str = br.readLine();
-            StringTokenizer st = new StringTokenizer(str, " ");
-
-            int N = Integer.parseInt(st.nextToken());
-            int[] arr = new int[N];
-            float sum = 0;
-            for(int j=0;j<N;j++){
-                arr[j] = Integer.parseInt(st.nextToken());
-                sum += arr[j];
-            }
-
-            float avg = sum / N;
-            float count = 0;
-            for (int j=0;j<N;j++){
-                if(arr[j]>avg) count++;
-            }
-            float result = count / N * 100;
-            System.out.printf("%.3f",result);
-            System.out.println("%\n");
-
-        }
-
-
     }
 }
