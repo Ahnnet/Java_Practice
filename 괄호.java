d11c38599b50dc9c84ba95fae0cdd0e9ef66be57
Hyoung Jin <drj9802@gmail.com>
2023-03-10 23:40:05 +0900

    p9012 기초(자료구조) - 괄호

---
+++ 괄호.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class 괄호 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0;i<n;i++){
+            String str = br.readLine();
+        }
+    }
+}
