ee494cc1f5c618603da34485aeeffec6b21cda93
Hyoung Jin <drj9802@gmail.com>
2023-03-11 01:03:59 +0900

    p9012 기초(자료구조) - 괄호

--- 괄호.java
+++ 괄호.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.util.Stack;
 public class 괄호 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,15 +11,21 @@ public class 괄호 {
 
         int n = Integer.parseInt(br.readLine());
 
+        loop:
         for(int i=0;i<n;i++){
             String str = br.readLine();
-            int lCount=0, rCount=0;
-
+            Stack<Character> stack = new Stack<>();
             for(int j=0;j<str.length();j++){
-                if(str.charAt(j)==('(')) lCount++;
-                else if(str.charAt(j)==(')')) rCount++;
+                char c = str.charAt(j);
+
+                if(c=='(') stack.push(c);
+                else if(stack.empty()) {
+                    bw.write("NO\n");
+                    continue loop;
+                }
+                else stack.pop();
             }
-            if(lCount==rCount) bw.write("YES\n");
+            if(stack.empty()) bw.write("YES\n");
             else bw.write("NO\n");
         }
         bw.flush();
