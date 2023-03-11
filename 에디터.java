15d8702ba98e7ea37644fc1be144000184e4d38b
Hyoung Jin <drj9802@gmail.com>
2023-03-12 01:06:15 +0900

    p1406 기초(자료구조) - 에디터

--- 에디터.java
+++ 에디터.java
@@ -5,7 +5,7 @@ import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class Main{
+public class 에디터{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
